4820d4cf28c0647a1435aafe8078c2fe98a9010d
EvgeniyMerlin <vocper@mail.ru>
2018-06-02 15:41:40 +0500

    add validToQestion and communications Qestion<User

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
-  validates :text, presence: true
+  belongs_to :user
+  validates :text, :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :questions
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
 end
